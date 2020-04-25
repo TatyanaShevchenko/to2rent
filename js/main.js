e15f078c8c0fec2e1162e9bb38e462bae9884de9
Tatyana Shevchenko <dafna2011@gmail.com>
2020-04-25 15:07:52 +0300

    hohoho

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('.slider').slick({
+nextArrow:'<button class="slick-arrow slick-next"><img src="images/next.svg" alt=""></button>',
+prevArrow:'<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt=""></button>',
+fade: true
+    });
+});
+
+
+
